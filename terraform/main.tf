921bf0c2e43d73037832a31402bf85a5e92cd441
Aaditya Desai <85244320+aaditya-desai1@users.noreply.github.com>
2025-03-30 10:59:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,12 @@
-provider "docker" {
-version = "~> 2.0"
-source = "kreuzwerker/docker"
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.0"
+    }
+  }
 }
+provider "docker" {}
 
 resource "docker_image" "node_app" {
   name         = "node-app"
